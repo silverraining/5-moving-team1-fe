f5d609ad09e90a9d9ef19a66be70885db4458f99
hay-oon <hayooncode@gmail.com>
2025-05-23 00:05:50 +0900

    chore: 깃 캐시파일 삭제후 커밋

--- src/api/axiosclient.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const apiClient = axios.create({
-  baseURL: "https://api.example.com",
-  timeout: 10000,
-});
-
-export default apiClient;
--- src/mock/browser.ts
+++
@@ -1,6 +0,0 @@
-import { setupWorker } from "msw/browser";
-import { handlers } from "./hendler";
-
-const isBrowser = typeof window !== "undefined";
-
-export const worker = isBrowser ? setupWorker(...handlers) : null;
