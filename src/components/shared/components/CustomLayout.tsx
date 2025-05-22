5f790c688a326c7797c8dae6ae77acc71aeccba1
김조순 <whtns611@naver.com>
2025-05-22 15:52:43 +0900

    feat : snackbar 추가

--- src/components/shared/components/CustomLayout.tsx
+++ src/components/shared/components/CustomLayout.tsx
@@ -5,15 +5,18 @@ import { ReactNode, useEffect, useState } from "react";
 import { Header } from "./header/Header";
 import { DarkModeToggle } from "./ColorModeToggle";
 import { usePathname } from "next/navigation";
+import { useSnackbar } from "@/src/hooks/snackBarHooks";
 
-type LayOutProps = {
+type CustomLayoutProps = {
   children: ReactNode;
 };
 
-export const LayOut = ({ children }: LayOutProps) => {
+export const CustomLayout = ({ children }: CustomLayoutProps) => {
   const [mounted, setMounted] = useState(false);
   const pathname = usePathname();
   const isMainPage = pathname === "/";
+  const { SnackbarComponent } = useSnackbar();
+
   useEffect(() => {
     setMounted(true);
   }, []);
@@ -29,7 +32,10 @@ export const LayOut = ({ children }: LayOutProps) => {
       bgcolor={isMainPage ? "#F5F5F5" : "transparent"}
     >
       <Header />
-      <Box px={["26px", "72px", "260px"]}>{children}</Box>
+      <Box px={["26px", "72px", "260px"]}>
+        {children}
+        {SnackbarComponent}
+      </Box>
       <DarkModeToggle />
     </Stack>
   );
