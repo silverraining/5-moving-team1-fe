638badd78f77ea9103d77bd62fdcefc6e5e39510
김조순 <whtns611@naver.com>
2025-05-22 13:39:07 +0900

    Feat : 필터 모달 추가 및 hook 추가

--- src/hooks/utill.ts
+++ src/hooks/utill.ts
@@ -53,3 +53,57 @@ export const useTab = () => {
 
   return { value, handleChange, setValue };
 };
+
+type OptionKey = "small" | "home" | "office";
+
+export const useFilterModal = () => {
+  const [open, setOpen] = useState(false);
+
+  const [checked, setChecked] = useState({
+    all: false,
+    small: false,
+    home: false,
+    office: false,
+  });
+
+  const openModal = useCallback(() => setOpen(true), []);
+  const closeModal = useCallback(() => setOpen(false), []);
+
+  const handleAllChange = (value: boolean) => {
+    setChecked({
+      all: value,
+      small: value,
+      home: value,
+      office: value,
+    });
+  };
+
+  const handleIndividualChange = (key: OptionKey, value: boolean) => {
+    const updated = {
+      ...checked,
+      [key]: value,
+    };
+    const allChecked = updated.small && updated.home && updated.office;
+    setChecked({
+      ...updated,
+      all: allChecked,
+    });
+  };
+
+  const indeterminate =
+    !checked.all && (checked.small || checked.home || checked.office);
+
+  const handleSubmitFilters = () => {
+    closeModal(); // 필요 시 닫기
+  };
+  return {
+    open,
+    openModal,
+    closeModal,
+    handleSubmitFilters,
+    checked,
+    indeterminate,
+    handleAllChange,
+    handleIndividualChange,
+  };
+};
---
+++ stories/FilterModal.stories.tsx
@@ -0,0 +1,75 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import { useFilterModal } from "@/src/hooks/utill";
+import FilterModal from "@/src/components/shared/components/modal/FilterModal";
+import { Button } from "@mui/material";
+
+const meta: Meta<typeof FilterModal> = {
+  title: "FilterModal",
+  component: FilterModal,
+  parameters: {
+    docs: {
+      description: {
+        component: "이사 유형 필터를 위한 모달 컴포넌트입니다.",
+      },
+    },
+  },
+  argTypes: {
+    onClose: { action: "close modal" },
+    onAllChange: { action: "전체선택 클릭" },
+    onIndividualChange: { action: "개별체크 클릭" },
+    onSubmit: { action: "조회하기 버튼 클릭" },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof FilterModal>;
+
+export const Base: Story = {
+  render: (args) => {
+    const {
+      open,
+      openModal,
+      closeModal,
+      checked,
+      indeterminate,
+      handleAllChange,
+      handleIndividualChange,
+      handleSubmitFilters,
+    } = useFilterModal();
+
+    const test = { all: 40, home: 20, small: 10, office: 10 };
+
+    return (
+      <>
+        <Button variant="contained" onClick={openModal}>
+          필터 열기
+        </Button>
+        <FilterModal
+          {...args}
+          open={open}
+          onClose={(...e) => {
+            args.onClose?.(...e);
+            closeModal();
+          }}
+          count={test}
+          checked={checked}
+          indeterminate={indeterminate}
+          onAllChange={(value) => {
+            args.onAllChange?.(value);
+            handleAllChange(value);
+          }}
+          onIndividualChange={(key, value) => {
+            args.onIndividualChange?.(key, value);
+            handleIndividualChange(key, value);
+          }}
+          onSubmit={(...e) => {
+            args.onSubmit?.(...e);
+            handleSubmitFilters();
+          }}
+        />
+      </>
+    );
+  },
+};
