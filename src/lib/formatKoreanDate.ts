e18f0a63ae230a5c8c2dc8c4355266fe853986b0
YOONMINHO <ujm4243@naver.com>
2025-05-23 20:49:08 +0900

    feat: lib(시간 및 콤파 표시)

--- src/lib/formatKoreanDate.ts
+++ src/lib/formatKoreanDate.ts
@@ -1,4 +1,7 @@
-export const formatKoreanDate = (input: string): string => {
+export const formatKoreanDate = (
+  input: string,
+  includeWeekday: boolean = true
+): string => {
   const date = new Date(input);
   const weekdays = ["일", "월", "화", "수", "목", "금", "토"];
 
@@ -7,5 +10,7 @@ export const formatKoreanDate = (input: string): string => {
   const day = String(date.getDate()).padStart(2, "0");
   const weekday = weekdays[date.getDay()];
 
-  return `${year}.${month}.${day}(${weekday})`;
+  return includeWeekday
+    ? `${year}.${month}.${day}(${weekday})`
+    : `${year}.${month}.${day}`;
 };
