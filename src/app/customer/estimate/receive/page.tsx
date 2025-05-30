963c28c1716b229d34b796fbfd69510532973a07
YOONMINHO <ujm4243@naver.com>
2025-05-30 16:53:09 +0900

    feat: 내 견적 관리 대기 중인 페이지

---
+++ src/app/customer/estimate/receive/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { Box } from "@mui/material";
+import ReceiveEstimate from "@/src/components/customer/estimate/ReceiveEstimate";
+import React from "react";
+
+export default function QuoteTabs() {
+  return (
+    <Box>
+      <ReceiveEstimate />
+    </Box>
+  );
+}
