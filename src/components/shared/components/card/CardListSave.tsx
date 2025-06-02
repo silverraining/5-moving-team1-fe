7056713f7824d0b380081f083d8abe342f48138d
silverraining <www.stella.choi@gmail.com>
2025-06-03 03:31:01 +0900

    feat: CardListSave, ChipCategory에 강제모바일사이즈 적용 훅(useResponsiveValue) 만들어서 적용

--- src/components/shared/components/card/CardListSave.tsx
+++ src/components/shared/components/card/CardListSave.tsx
@@ -2,46 +2,75 @@ import { Box, Typography } from "@mui/material";
 import { ChipCategory } from "../chip/ChipCategory";
 import { CardData } from "@/src/types/card";
 import Image from "next/image";
-
+import { useResponsiveValue } from "@/src/hooks/useResponsiveValue";
 interface CardProps {
   data: CardData;
   onLikeClick?: () => void;
+  forceMobileSize?: boolean;
 }
 
-export const CardListSave = ({ data, onLikeClick }: CardProps) => {
+export const CardListSave = ({
+  data,
+  onLikeClick,
+  forceMobileSize = false,
+}: CardProps) => {
+  const responsive = useResponsiveValue(forceMobileSize);
+
   return (
     <Box
       display="flex"
       flexDirection="column"
       justifyContent="space-between"
       border="0.5px solid #F2F2F2"
-      width={[327, 600, 688]}
-      height={[150, 150, 202]}
+      width={responsive([327, 600, 688])}
+      height="auto"
       bgcolor="#FFFFFF"
       borderRadius="16px"
-      padding={["16px 14px", "16px 14px", "20px 24px"]}
+      padding={responsive(["16px 14px", "16px 14px", "20px 24px"])}
       boxShadow="2px 2px 10px 0px #DCDCDC24, -2px -2px 10px 0px #DCDCDC24"
       boxSizing={"border-box"}
     >
       <Box>
-        <Box display="flex" flexDirection="row" gap={["8px", "12px"]}>
+        <Box
+          display="flex"
+          flexDirection="row"
+          gap={responsive(["8px", "12px", "12px"])}
+        >
           {data.types.map((type, index) => (
-            <ChipCategory key={index} type={type} />
+            <ChipCategory
+              key={index}
+              type={type}
+              forceMobileSize={forceMobileSize}
+            />
           ))}
         </Box>
       </Box>
-
+      <Typography
+        sx={(theme) => ({
+          fontSize: responsive([14, 14, 18]),
+          lineHeight: responsive(["24px", "24px", "26px"]),
+          fontWeight: 600,
+          color: theme.palette.Black[300],
+          paddingY: responsive(["10px", "10px", "16px"]),
+        })}
+      >
+        {data.message}
+      </Typography>
       {/* 아래 */}
       <Box
         display="flex"
         border="1px solid #F2F2F2"
         bgcolor="#FFFFFF"
-        padding={["10px", "10px", "16px 18px"]}
+        padding={responsive(["10px", "10px", "16px 18px"])}
         boxShadow="4px 4px 16px 0px #E9E9E91A"
-        gap={["12px", "12px", "24px"]}
-        borderRadius={"6px"}
+        gap={responsive(["12px", "12px", "24px"])}
+        borderRadius="6px"
       >
-        <Box width={[46, 46, 56]} height={[46, 46, 56]} position="relative">
+        <Box
+          width={responsive([46, 46, 56])}
+          height={responsive([46, 46, 56])}
+          position="relative"
+        >
           <Image
             src={data.imgSrc}
             alt={"프로필 이미지"}
@@ -61,8 +90,8 @@ export const CardListSave = ({ data, onLikeClick }: CardProps) => {
           >
             <Typography
               sx={(theme) => ({
-                fontSize: [14, 14, 18],
-                lineHeight: ["24px", "24px", "26px"],
+                fontSize: responsive([14, 14, 18]),
+                lineHeight: responsive(["24px", "24px", "26px"]),
                 fontWeight: 600,
                 color: theme.palette.Black[300],
               })}
@@ -84,8 +113,8 @@ export const CardListSave = ({ data, onLikeClick }: CardProps) => {
               />
               <Typography
                 sx={(theme) => ({
-                  fontSize: [13, 13, 18],
-                  lineHeight: ["22px", "22px", "26px"],
+                  fontSize: responsive([13, 13, 18]),
+                  lineHeight: responsive(["22px", "22px", "26px"]),
                   fontWeight: 500,
                   color: theme.palette.PrimaryBlue[400],
                 })}
@@ -97,26 +126,27 @@ export const CardListSave = ({ data, onLikeClick }: CardProps) => {
           <Box
             display="flex"
             flexDirection="row"
-            gap={"9.5px"}
+            gap="9.5px"
             alignItems="center"
             flexGrow={1}
-            justifyContent={["space-between", "flex-start"]}
+            justifyContent={responsive([
+              "space-between",
+              "flex-start",
+              "flex-start",
+            ])}
           >
-            <Box
-              display="flex"
-              justifyContent="space-between"
-              alignItems="center"
-            >
+            {/* 별점 */}
+            <Box display="flex" alignItems="center" gap="4px">
               <Image
                 src="/Images/star/star_active.svg"
-                alt="별점 사진"
+                alt="별점"
                 width={20}
                 height={20}
               />
               <Typography
                 sx={(theme) => ({
-                  fontSize: [13, 13, 16],
-                  lineHeight: ["22px", "22px", "26px"],
+                  fontSize: responsive([13, 13, 16]),
+                  lineHeight: responsive(["22px", "22px", "26px"]),
                   fontWeight: 500,
                   color: theme.palette.Black[300],
                 })}
@@ -125,8 +155,8 @@ export const CardListSave = ({ data, onLikeClick }: CardProps) => {
               </Typography>
               <Typography
                 sx={(theme) => ({
-                  fontSize: [13, 13, 16],
-                  lineHeight: ["22px", "22px", "26px"],
+                  fontSize: responsive([13, 13, 16]),
+                  lineHeight: responsive(["22px", "22px", "26px"]),
                   fontWeight: 500,
                   color: theme.palette.Grayscale[300],
                 })}
@@ -135,46 +165,52 @@ export const CardListSave = ({ data, onLikeClick }: CardProps) => {
               </Typography>
             </Box>
             <Box height={14} border={"1px solid #E6E6E6"}></Box>
-            <Box display="flex">
+            {/* 경력 */}
+            <Box display="flex" gap="4px">
               <Typography
                 sx={(theme) => ({
-                  fontSize: [13, 13, 16],
-                  lineHeight: ["22px", "22px", "26px"],
+                  fontSize: responsive([13, 13, 16]),
+                  lineHeight: responsive(["22px", "22px", "26px"]),
                   fontWeight: 500,
                   color: theme.palette.Grayscale[300],
+                  whiteSpace: "nowrap",
                 })}
               >
                 경력
               </Typography>
               <Typography
                 sx={(theme) => ({
-                  fontSize: [13, 13, 16],
-                  lineHeight: ["22px", "22px", "26px"],
+                  fontSize: responsive([13, 13, 16]),
+                  lineHeight: responsive(["22px", "22px", "26px"]),
                   fontWeight: 500,
                   color: theme.palette.Black[300],
+                  whiteSpace: "nowrap",
                 })}
               >
                 {data.career}년
               </Typography>
             </Box>
             <Box height={14} border={"1px solid #E6E6E6"}></Box>
-            <Box display="flex">
+            {/* 확정 */}
+            <Box display="flex" gap="4px">
               <Typography
                 sx={(theme) => ({
-                  fontSize: [13, 13, 16],
-                  lineHeight: ["22px", "22px", "26px"],
+                  fontSize: responsive([13, 13, 16]),
+                  lineHeight: responsive(["22px", "22px", "26px"]),
                   fontWeight: 500,
                   color: theme.palette.Black[300],
+                  whiteSpace: "nowrap",
                 })}
               >
-                {data.confirm}
+                {data.confirm}건
               </Typography>
               <Typography
                 sx={(theme) => ({
-                  fontSize: [13, 13, 16],
-                  lineHeight: ["22px", "22px", "26px"],
+                  fontSize: responsive([13, 13, 16]),
+                  lineHeight: responsive(["22px", "22px", "26px"]),
                   fontWeight: 500,
                   color: theme.palette.Grayscale[300],
+                  whiteSpace: "nowrap",
                 })}
               >
                 확정
