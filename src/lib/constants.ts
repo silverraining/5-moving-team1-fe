1ed30e838dd552e75f38881324a3b06e3c16fdc9
김조순 <whtns611@naver.com>
2025-06-03 17:17:57 +0900

    feat : PATH 상수 추가 및 수정 , 헤더에 쓸 상수 추가

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -24,62 +24,77 @@ export const RegionType = [
 export type Region = (typeof RegionType)[number];
 
 export const PATH = {
-  /**메인페이지 */
+  /** 메인 페이지 (/) */
   main: "/",
-  /**일반 유저 로그인 페이지 */
+
+  /** 일반 유저 로그인 페이지 (/auth/user/login) */
   userLogin: "/auth/user/login",
-  /**기사로그인 페이지 */
+
+  /** 기사 로그인 페이지 (/auth/mover/login) */
   moverLogin: "/auth/mover/login",
-  /**일반유저 회원가입 페이지 */
+
+  /** 일반 유저 회원가입 페이지 (/auth/user/signup) */
   userSignup: "/auth/user/signup",
-  /**기사 회원가입 페이지 */
+
+  /** 기사 회원가입 페이지 (/auth/mover/signup) */
   moverSignup: "/auth/mover/signup",
-  /**일반유저 프로필 등록 페이지*/
-  userProfileResister: "/customer/profile/register",
-  /**일반유저 프로필 수정 페이지*/
-  userProfileEdit: "/customer/profile/edit",
-  /**기사 프로필 등록 페이지*/
-  moverProfileResister: "/mover/profile/register",
-  /**기사 프로필 수정 페이지*/
+
+  /** 기사 프로필 등록 페이지 (/mover/profile/register) */
+  moverProfileRegister: "/mover/profile/register",
+
+  /** 기사 프로필 수정 페이지 (/mover/profile/edit) */
   moverProfileEdit: "/mover/profile/edit",
-  /**기사 상세 페이지
-   * id:string
-   * 아이디 값만 넣어주세용
-   */
-  moverDtail: (id: string) => `/customer/moverList/${id}`,
-  /**기사 리스트 페이지*/
-  moverList: "/customer/moverList",
-  /**견적 요청 페이지*/
-  customerRequest: "/customer/request",
-  /**기사가 받은 견적 페이지*/
+
+  /** 기사가 받은 견적 리스트 페이지 (/mover/requests) */
   moverRequest: "/mover/requests",
-  /**내가 만든 견적 보기 페이지 */
-  userEstimate: "/customer/estimate/pending",
-  /**내가 만든 견적 상세 보기 페이지
-   * id : string
-   */
-  userEstimateDetail: (id: string) => `/customer/estimate/pending/${id}`,
-  /** 받았던 견적 보기 페이지 */
-  userEstimateReceive: "/customer/estimate/receive",
-  /** 받았던 견적 상세 보기 페이지
-   * id : string
-   */
-  userEstimateReceiveDetail: (id: string) => `/customer/estimate/receive/${id}`,
-  /**내견적관리-확정 견적 */
-  moverEstimateComfirm: "/mover/estimate/confirm",
-  /**내견적관리-확정 견적 상세보기 */
-  moverEstimateComfirmDetail: (id: string) => `/mover/estimate/confirm/${id}`,
-  /**내견적관리-반려 견적 */
+
+  /** 기사의 확정된 견적 리스트 페이지 (/mover/estimate/confirm) */
+  moverEstimateConfirm: "/mover/estimate/confirm",
+
+  /** 기사의 확정된 견적 상세 페이지 (/mover/estimate/confirm/:id) */
+  moverEstimateConfirmDetail: (id: string) => `/mover/estimate/confirm/${id}`,
+
+  /** 기사의 반려된 견적 리스트 페이지 (/mover/estimate/reject) */
   moverEstimateReject: "/mover/estimate/reject",
-  /**찜한 기사 페이지 */
+
+  /** 기사 상세 페이지 (/customer/moverList/:id) */
+  moverDetail: (id: string) => `/customer/moverlist/${id}`,
+
+  /** 기사 리스트 페이지 (/customer/moverList) */
+  moverList: "/customer/moverlist",
+
+  /** 기사 리뷰 페이지 (/mover/reviews) */
+  moverReview: "/mover/reviews",
+
+  /** 견적 요청 페이지 (/customer/request) */
+  userRequest: "/customer/request",
+
+  /** 내가 만든 견적 리스트 페이지 (/customer/estimate/pending) */
+  userEstimate: "/customer/estimate/pending",
+
+  /** 일반 유저 프로필 등록 페이지 (/customer/profile/register) */
+  userProfileRegister: "/customer/profile/register",
+
+  /** 일반 유저 프로필 수정 페이지 (/customer/profile/edit) */
+  userProfileEdit: "/customer/profile/edit",
+
+  /** 내가 만든 견적 상세 페이지 (/customer/estimate/pending/:id) */
+  userEstimateDetail: (id: string) => `/customer/estimate/pending/${id}`,
+
+  /** 받았던 견적 리스트 페이지 (/customer/estimate/receive) */
+  userEstimateReceive: "/customer/estimate/receive",
+
+  /** 받았던 견적 상세 페이지 (/customer/estimate/receive/:id) */
+  userEstimateReceiveDetail: (id: string) => `/customer/estimate/receive/${id}`,
+
+  /** 일반 유저의 찜한 기사 리스트 페이지 (/customer/wishlist) */
   userWishlist: "/customer/wishlist",
-  /**일반유저 리뷰 페이지 */
+
+  /** 일반 유저 리뷰 페이지 (/customer/reviews) */
   userReview: "/customer/reviews",
-  /**기사 리뷰 페이지 */
-  moveRreview: "/mover/reviews",
 };
 
 export const API_BASE_URL =
   process.env.NODE_ENV === "development"
-    ? "http://localhost:3000"
+    ? "http://localhost:5000/api"
     : process.env.API_URL;
