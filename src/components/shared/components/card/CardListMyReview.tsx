335b566255284deba1ee79a0d1ee09ef17a4fcc9
YOONMINHO <ujm4243@naver.com>
2025-06-04 15:22:20 +0900

    fix: 디자인 수정

--- src/components/shared/components/card/CardListMyReview.tsx
+++ src/components/shared/components/card/CardListMyReview.tsx
@@ -25,7 +25,13 @@ export const CardListMyReview = ({ data }: CardProps) => {
       boxShadow="2px 2px 10px 0px #DCDCDC24, -2px -2px 10px 0px #DCDCDC24"
       boxSizing={"border-box"}
     >
-      <Box display="flex" flexDirection="row" gap={["14px", "16px"]}>
+      <Box
+        display="flex"
+        flexDirection="row"
+        gap={["14px", "16px"]}
+        justifyContent="space-between"
+        alignItems="center"
+      >
         <Box display="flex" flexDirection="row" gap={["8px", "12px"]}>
           {data.types.map((type, index) => (
             <ChipCategory key={index} type={type} />
@@ -34,11 +40,11 @@ export const CardListMyReview = ({ data }: CardProps) => {
         <Box display="flex">
           <Typography
             sx={(theme) => ({
-              fontSize: [14, 14, 24],
-              lineHeight: ["24px", "24px", "32px"],
-              fontWeight: 600,
-              color: theme.palette.Black[300],
-              display: ["none", "block", "block"],
+              fontSize: [12, 12, 18],
+              lineHeight: ["18px", "18px", "26px"],
+              fontWeight: 400,
+              color: theme.palette.Grayscale[300],
+              display: ["none", "none", "block"],
             })}
           >
             작성일 {formatKoreanDate(data.date ?? "", false)}
@@ -49,13 +55,14 @@ export const CardListMyReview = ({ data }: CardProps) => {
       {/* 아래 */}
       <Box
         display="flex"
-        border={["0px", "0px", "1px solid"]}
-        borderColor={COLORS.Line[100]}
-        bgcolor="#FFFFFF"
+        border={["0px", "0px", "1px solid #f2f2f2"]}
         padding={["10px", "10px", "16px 18px"]}
         boxShadow="4px 4px 16px 0px #E9E9E91A"
         gap={["12px", "12px", "24px"]}
         borderRadius={"1px"}
+        sx={(theme) => ({
+          backgroundColor: theme.palette.White[100],
+        })}
       >
         <Box width={[46, 46, 96]} height={[46, 46, 96]} position="relative">
           <Image
@@ -74,7 +81,9 @@ export const CardListMyReview = ({ data }: CardProps) => {
           <Box
             display="flex"
             flexDirection="row"
-            justifyContent="space-between"
+            justifyContent="flex-start"
+            alignItems="center"
+            gap={"6px"}
           >
             <Typography
               sx={(theme) => ({
@@ -86,7 +95,7 @@ export const CardListMyReview = ({ data }: CardProps) => {
             >
               {data.name} 기사님
             </Typography>
-            <Box display="flex" gap="4px">
+            <Box display={["flex", "flex", "none"]} gap="4px">
               {Array.from({ length: data.review ?? 0 }).map((_, i) => (
                 <Box
                   key={i}
@@ -131,7 +140,7 @@ export const CardListMyReview = ({ data }: CardProps) => {
                   color: theme.palette.Black[300],
                 })}
               >
-                {data.career}년
+                {formatKoreanDate(data.movingDay ?? "", false)}
               </Typography>
             </Box>
             <Box height={14} border={"1px solid #E6E6E6"}></Box>
@@ -158,6 +167,23 @@ export const CardListMyReview = ({ data }: CardProps) => {
               </Typography>
             </Box>
           </Box>
+          <Box display={["none", "none", "flex"]} gap="4px">
+            {Array.from({ length: data.review ?? 0 }).map((_, i) => (
+              <Box
+                key={i}
+                position="relative"
+                width={[20, 20, 24]}
+                height={[20, 20, 24]}
+              >
+                <Image
+                  src="/Images/star/star_active.svg"
+                  alt="별점 사진"
+                  fill
+                  style={{ objectFit: "contain" }}
+                />
+              </Box>
+            ))}
+          </Box>
         </Box>
       </Box>
       <Box display="flex" justifyContent="space-between" flexDirection="column">
@@ -172,14 +198,13 @@ export const CardListMyReview = ({ data }: CardProps) => {
           {data.writeReview}
         </Typography>
         <Typography
-          display={"flex"}
+          display={["flex", "flex", "none"]}
           justifyContent="flex-end"
           sx={(theme) => ({
             fontSize: [14, 14, 18],
             lineHeight: ["24px", "24px", "26px"],
             fontWeight: 400,
             color: theme.palette.Grayscale[300],
-            display: ["block", "none", "none"],
           })}
         >
           작성일 {formatKoreanDate(data.date ?? "", false)}
