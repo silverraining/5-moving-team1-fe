93f7870cd645092757e206418c86bf53c4cf2e32
sejeong <ygn06024@naver.com>
2025-06-13 15:47:16 +0900

    refactor: 견적요청_진행중인 경우 페이지- 버튼을 눌렀을 때 라우팅될 페이지가 기획에 없어서 주석처리(추후 추가 예정)

--- src/components/customer/request/steps/InProgressPage.tsx
+++ src/components/customer/request/steps/InProgressPage.tsx
@@ -1,20 +1,14 @@
 "use client";
 
 import Image from "next/image";
-import {
-  Stack,
-  Typography,
-  Button,
-  useTheme,
-  useMediaQuery,
-} from "@mui/material";
-import { useRouter } from "next/navigation";
+import { Stack, Typography, useTheme, useMediaQuery } from "@mui/material";
+// import { useRouter } from "next/navigation";
 
 export default function InProgressPage() {
   const theme = useTheme();
   const isSmall = useMediaQuery(theme.breakpoints.down("tablet"));
 
-  const router = useRouter();
+  // const router = useRouter();
 
   return (
     <>
@@ -42,7 +36,8 @@ export default function InProgressPage() {
           <br />
           진행 중인 이사 완료 후 새로운 견적을 받아보세요.
         </Typography>
-        <Button
+        {/* 버튼을 눌렀을 때 이동할 기사님에게 견적받지 않은 견적도 확인할 수 있는 페이지가 없어서 일단 없음 처리 */}
+        {/* <Button
           onClick={() => router.push("/customer/estimate/pending")}
           sx={{
             height: isSmall ? "54px" : "64px",
@@ -61,7 +56,7 @@ export default function InProgressPage() {
           >
             받은 견적 보러가기
           </Typography>
-        </Button>
+        </Button> */}
       </Stack>
     </>
   );
