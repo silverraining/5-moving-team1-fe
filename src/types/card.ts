58ab4417b762b4582b5df3fa75a144ffe4566920
silverraining <www.stella.choi@gmail.com>
2025-06-22 07:41:43 +0900

    feat: 매핑 관련 상수, 함수 추가 , path 추가, 카드데이터에 chips, id 추가

--- src/types/card.ts
+++ src/types/card.ts
@@ -12,6 +12,7 @@ export interface ChipData {
 }
 
 export interface CardData {
+  id?: string;
   types: ServiceType[];
   message: string;
   imgSrc: string;
@@ -23,6 +24,7 @@ export interface CardData {
   career: number;
   confirm: number;
   address: string[];
+  chips?: ChipData[];
 }
 
 export type likeMoverListResItem = {
