5d15b69f9c3b70dba6ab86d375c93fb185764e27
김조순 <whtns611@naver.com>
2025-06-22 05:05:35 +0900

    feat : 리뷰 api 추가 및 api 연결

--- src/api/review/hooks.ts
+++ src/api/review/hooks.ts
@@ -1,5 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
-import { getMoverReviews } from "./api";
+import {
+  getCompletedReviews,
+  getMoverReviews,
+  getWriteReviewList,
+} from "./api";
 
 export const useMoverReviews = (
   moverId: string,
@@ -9,5 +13,19 @@ export const useMoverReviews = (
   return useQuery({
     queryKey: ["moverReviews", moverId, page, take],
     queryFn: () => getMoverReviews(moverId, page, take),
+  });
+};
+
+export const useWriteReviewsList = (page: number, take: number) => {
+  return useQuery({
+    queryKey: ["writeReviewsList", page, take],
+    queryFn: () => getWriteReviewList(page, take),
+  });
+};
+
+export const useCompletedReviews = (page: number, take: number) => {
+  return useQuery({
+    queryKey: ["completedReviews", page, take],
+    queryFn: () => getCompletedReviews(page, take),
   });
 };
